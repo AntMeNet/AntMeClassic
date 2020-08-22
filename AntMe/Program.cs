5efc56b0ce96e6ac37ac8c38a11b04c383bf03f1
Tom Wendel <tom.wendel@antme.net>
2020-08-22 02:10:29 +0200

    Add a x86/x64 switch to the main application

--- AntMe/Program.cs
+++ AntMe/Program.cs
@@ -1,20 +1,59 @@
 using System;
 using System.Globalization;
+using System.IO;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Windows.Forms;
 
 using AntMe.Gui.Properties;
 
-namespace AntMe.Gui {
-    internal sealed class Program {
+namespace AntMe.Gui
+{
+    internal sealed class Program
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetDefaultDllDirectories(int directoryFlags);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        static extern void AddDllDirectory(string lpPathName);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetDllDirectory(string lpPathName);
+
+        const int LOAD_LIBRARY_SEARCH_DEFAULT_DIRS = 0x00001000;
+
         [STAThread]
-        public static void Main(string[] parameter) {
+        public static void Main(string[] parameter)
+        {
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                try
+                {
+                    SetDefaultDllDirectories(LOAD_LIBRARY_SEARCH_DEFAULT_DIRS);
+                    AddDllDirectory(Path.Combine(
+                        AppDomain.CurrentDomain.BaseDirectory,
+                        Environment.Is64BitProcess ? "x64" : "x86"
+                    ));
+                }
+                catch
+                {
+                    // Pre-Windows 7, KB2533623 
+                    SetDllDirectory(Path.Combine(
+                        AppDomain.CurrentDomain.BaseDirectory,
+                        Environment.Is64BitProcess ? "x64" : "x86"
+                    ));
+                }
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
             bool restart = true;
 
-            while (restart) {
+            while (restart)
+            {
 
                 // Language-Settings
                 switch (Settings.Default.culture)
@@ -30,7 +69,8 @@ namespace AntMe.Gui {
                 }
 
                 // Run
-                using (Main form = new Main(parameter)) {
+                using (Main form = new Main(parameter))
+                {
                     Application.Run(form);
                     restart = form.Restart;
                 }
