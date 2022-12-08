5234749f465c9adb26649aa268d1ddc13191c1f8
al3xander <git@trusted-security.net>
2022-12-08 11:42:10 +0100

    adding additional functionality to Insect.cs according to german Insekt.cs

--- SimulationCore/English/Insect.cs
+++ SimulationCore/English/Insect.cs
@@ -12,17 +12,56 @@ namespace AntMe.English
         /// <summary>
         /// Returns the unique ID of this insect.
         /// </summary>
-        public override int Id
-        {
-            get { return ((CoreInsect)Baseitem).Id; }
-        }
+        public override int Id => ((CoreInsect)Baseitem).Id;
 
         /// <summary>
-        /// Delivers the current energy of this ant.
+        /// Returns the current energy of this insect.
         /// </summary>
-        public int CurrentEnergy
-        {
-            get { return ((CoreInsect)Baseitem).currentEnergyCoreInsect; }
-        }
+        public int CurrentEnergy => ((CoreInsect)Baseitem).currentEnergyCoreInsect;
+
+        /// <summary>
+        /// Returns the current speed of the insect.
+        /// </summary>
+        public int CurrentSpeed => ((CoreInsect)Baseitem).CurrentSpeedCoreInsect;
+
+        /// <summary>
+        /// Returns the attack strength of the insect.
+        /// </summary>
+        public int AttackStrength => ((CoreInsect)Baseitem).AttackStrengthCoreInsect;
+
+        /// <summary>
+        /// Returns the rotation speed of the insect.
+        /// </summary>
+        public int RotationSpeed => ((CoreInsect)Baseitem).RotationSpeedCoreInsect;
+
+        /// <summary>
+        /// Returns the maximum energy of the insect.
+        /// </summary>
+        public int MaximumEnergy => ((CoreInsect)Baseitem).MaximumEnergyCoreInsect;
+
+        /// <summary>
+        /// Returns the maximum speed of the insect.
+        /// </summary>
+        public int MaximumSpeed => ((CoreInsect)Baseitem).MaximumSpeedCoreInsect;
+
+        /// <summary>
+        /// Returns the view range of the insect.
+        /// </summary>
+        public int ViewRange => ((CoreInsect)Baseitem).ViewRangeCoreInsect;
+
+        /// <summary>
+        /// Returns the residual angle of the insect.
+        /// </summary>
+        public int ResidualAngle => ((CoreInsect)Baseitem).ResidualAngle;
+
+        /// <summary>
+        /// Returns the direction the insect is heading to.
+        /// </summary>
+        public int Direction => ((CoreInsect)Baseitem).GetDirectionCoreInsect();
+
+        /// <summary>
+        /// Returns the distance the insect has to cover.
+        /// </summary>
+        public int DistanceToDestination => ((CoreInsect)Baseitem).DistanceToDestinationCoreInsect;
     }
 }
